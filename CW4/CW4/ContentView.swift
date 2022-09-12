d8dee6672a7e26e7a7baf9666d3641112f343fda
deemfaisal <dfaisal07@gmail.com>
2022-09-12 20:27:27 +0300

    Project4

---
+++ CW4/CW4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CW4
+//
+//  Created by Deem on 12/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
